4bf7a0d290e785e8b70d80cbd5427e9ff1899fe6
pariza1305 <parizagoyal@gmail.com>
2025-03-12 09:49:17 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,12 @@
+def is_narc(n):
+    """Check if a number is a Narcissistic number."""
+    num_str = str(n)
+    num_digits = len(num_str)
+    sum_of_powers = sum(int(digit) ** num_digits for digit in num_str)
+    return sum_of_powers == n
+def print_narcis_numbers(start, end):
+    """Print all Narcissistic numbers in a given range."""
+    for num in range(start, end + 1):
+        if is_narc(num):
+            print(num)
+print_narcis_numbers(1000, 5000)
